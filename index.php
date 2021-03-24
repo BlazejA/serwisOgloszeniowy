10972ae3d48bed7dae225a22560f5433103ed3c7
Blazej <a.blazej8@gmail.com>
2021-03-24 20:28:45 +0100

    2 pages done
    done: main page, add ad
    start doing add account

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serwis ogłoszeń</title>
+    <link rel="shortcut icon" href="img/logo.png" type="image">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+        <nav class="navbar navbar-light navbar-expand-md fixed-top" style="background-color: #e6e6e6;">
+
+            <a href="#" class="navbar-brand order-first ms-1">
+                <img src="img/logo.png" alt="" width="70px" height="70px" class="d-inline-block">
+                 Joszczymbiok
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu"
+            aria-controls="mainmenu" aria-expanded="false" aria-label="przełacznik nawigacji">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div id="mainmenu" class="collapse navbar-collapse order-last">
+                
+                <ul class="navbar-nav ms-auto me-2">
+                    <li class="nav-item active">
+                        <a href="#" class="nav-link animatedButton active">STRONA GŁÓWNA</a>
+                    </li>
+                    <li class="nav-item mx-1">
+                        <a href="#" class="nav-link animatedButton">KONTO</a>
+                    </li>
+                    <li class="nav-item mx-1">
+                        <a href="addad.html" class="nav-link animatedButton">DODAJ OGŁOSZENIE</a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </nav>
+    </header>
+
+    <div class="container page">
+        
+        <div class="pagenav">
+            <div>
+                kategoria: telefony
+            </div>
+        </div>
+
+        <div class="ads"> 
+            <div class="row adbox">                
+                    <div class="col-3">
+                        <img src="telefon.jpg" alt="Zdjecie oferty" class="adsimg"/> 
+                    </div>
+                    <div class="col-7" style="margin-top: 10px;">
+                        <div class="adtitle">
+                            <a href="itemPage">Sprzedam telefon</a>
+                        </div> 
+                        <div class="adsdesc">
+                            Sprzedam ten telefon bo mnie denerwuje Sprzedam ten telefon bo mnie denerwuje Sprzedam ten telefon bo mnie denerwuje
+                        </div>
+                    </div>  
+                    <div class="col-2" style="margin-top: 10px;">
+                        <div class="adsprice">
+                            1500 PLN                            
+                        </div>
+                        <div class="adsname">                        
+                            Janek<br>
+                            Opolska 12
+                        </div>
+                    </div> 
+            </div>
+            <div class="row adbox">                
+                    <div class="col-3">
+                        <img src="telefon.jpg" alt="Zdjecie oferty" class="adsimg"/> 
+                    </div>
+                    <div class="col-7" style="margin-top: 10px;">
+                        <div class="adtitle">
+                            <a href="itemPage">Sprzedam telefon</a>
+                        </div> 
+                        <div class="adsdesc">
+                            Sprzedam ten telefon bo mnie denerwuje Sprzedam ten telefon bo mnie denerwuje Sprzedam ten telefon bo mnie denerwuje
+                        </div>
+                    </div>  
+                    <div class="col-2" style="margin-top: 10px;">
+                        <div class="adsprice">
+                            1500 PLN                            
+                        </div>
+                        <div class="adsname">                        
+                            Janek<br>
+                            Opolska 12
+                        </div>
+                    </div> 
+            </div>
+            <div class="row adbox">                
+                    <div class="col-3">
+                        <img src="telefon.jpg" alt="Zdjecie oferty" class="adsimg"/> 
+                    </div>
+                    <div class="col-7" style="margin-top: 10px;">
+                        <div class="adtitle">
+                            <a href="itemPage">Sprzedam telefon</a>
+                        </div> 
+                        <div class="adsdesc">
+                            Sprzedam ten telefon bo mnie denerwuje Sprzedam ten telefon bo mnie denerwuje Sprzedam ten telefon bo mnie denerwuje
+                        </div>
+                    </div>  
+                    <div class="col-2" style="margin-top: 10px;">
+                        <div class="adsprice">
+                            1500 PLN                            
+                        </div>
+                        <div class="adsname">                        
+                            Janek<br>
+                            Opolska 12
+                        </div>
+                    </div> 
+            </div>
+            <div class="row adbox">                
+                    <div class="col-3">
+                        <img src="telefon.jpg" alt="Zdjecie oferty" class="adsimg"/> 
+                    </div>
+                    <div class="col-7" style="margin-top: 10px;">
+                        <div class="adtitle">
+                            <a href="itemPage">Sprzedam telefon</a>
+                        </div> 
+                        <div class="adsdesc">
+                            Sprzedam ten telefon bo mnie denerwuje Sprzedam ten telefon bo mnie denerwuje Sprzedam ten telefon bo mnie denerwuje
+                        </div>
+                    </div>  
+                    <div class="col-2" style="margin-top: 10px;">
+                        <div class="adsprice">
+                            1500 PLN                            
+                        </div>
+                        <div class="adsname">                        
+                            Janek<br>
+                            Opolska 12
+                        </div>
+                    </div> 
+            </div>
+            <div class="row adbox">                
+                    <div class="col-3">
+                        <img src="telefon.jpg" alt="Zdjecie oferty" class="adsimg"/> 
+                    </div>
+                    <div class="col-7" style="margin-top: 10px;">
+                        <div class="adtitle">
+                            <a href="itemPage">Sprzedam telefon</a>
+                        </div> 
+                        <div class="adsdesc">
+                            Sprzedam ten telefon bo mnie denerwuje Sprzedam ten telefon bo mnie denerwuje Sprzedam ten telefon bo mnie denerwuje
+                        </div>
+                    </div>  
+                    <div class="col-2" style="margin-top: 10px;">
+                        <div class="adsprice">
+                            1500 PLN                            
+                        </div>
+                        <div class="adsname">                        
+                            Janek<br>
+                            Opolska 12
+                        </div>
+                    </div> 
+            </div>
+
+        </div>
+
+    </div>
+    
+
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+
+    </body>
+</html>
